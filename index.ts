dc7221b81fd97de226c6f8cc9bc4f36a230f2d48
zakiabashir <nshafeys0@gmail.com>
2024-05-08 13:33:18 -0700

    add a calculator ascii art

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+console.log(`
+_________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+`)
 const answer = await inquirer.prompt([
   { message: "enter your first number", type: "number", name: "firstnumber" },
   { message: "enter your second number", type: "number", name: "secondnumber" },
